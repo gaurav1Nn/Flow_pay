10a8f6ff2bd82fbc058639901b3c5f5f8a8a2193
gaurav1Nn <gauravnilawar26@gmail.com>
2025-07-02 14:39:58 +0530

    code added

---
+++ Flowpay-main/apps/user-app/app/layout.tsx
@@ -0,0 +1,39 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { Providers } from '../provider';
+import ClientWrapper from '../ClientWrapper';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import LenisProvider from '../providers/LenisProvider';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'FlowPay',
+  description: 'FlowPay a simulated wallet app',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element {
+  const showAppbarAndFooter = true; // default value
+
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>
+          <ClientWrapper>
+            <SpeedInsights />
+            <Analytics />
+            <LenisProvider>
+            {children}
+             </LenisProvider>
+          </ClientWrapper>
+        </Providers>
+      </body>
+    </html>
+  );
+}
